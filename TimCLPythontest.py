14cac3af9f8f3551d82dcccdde46c506ba02eb21
Timothy Walker <walker.7.tw@gmail.com>
2022-05-26 14:35:15 -0400

    Test Python File Code Louisville

---
+++ TimCLPythontest.py
@@ -0,0 +1,13 @@
+print("hello world!")
+
+vehicleMake = ["Ford","Chevrolet","Dodge","KIA","Nissan","Honda","Cadillac","Hyundai"]
+
+print(vehicleMake[3])
+
+vehicleType = {"F150": "pickup", "Malibu": "sedan"}
+
+print(vehicleType["Malibu"])
+
+vehicleSize = ("Truck","Sedan","SUV","compact")
+
+print(vehicleSize[2])
